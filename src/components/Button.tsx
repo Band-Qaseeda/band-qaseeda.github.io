db5a063df0eb4039d37da648267efd9a5b3ed386
saadalamin <saadalamin2004@gmail.com>
2024-09-18 19:02:24 +0600

    .

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ const Button = (props: any) => {
     <button
       {...props}
       className={
-        "bg-accent text-primary py-2 px-4 md:py-4 md:px-6 hover:bg-accent/80 transition-all duration-100 border border-primary/20 flex gap-1.5 md:gap-2 items-center rounded-lg font-medium text-md md:text-lg hover:border-transparent " +
+        "bg-accent text-primary py-2 px-4 md:py-3.5 md:px-8 hover:bg-accent/80 transition-all duration-100 border border-primary/20 flex gap-1.5 md:gap-2 items-center rounded-lg font-medium text-md md:text-lg hover:border-transparent " +
         props?.className
       }
     >
